65af7b1c905255f5fb8cc081b5efe178d67e4a6a
Helcio Itiyama <helcio.itiyama@gmail.com>
2020-07-08 12:46:44 -0300

    change cors

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: process.env.APP_URL,
+    origin: "http://localhost:3000",
   })
 );
 
